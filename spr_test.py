251f58f00480880c112821296a0bf179d91cb9e6
Michele <michele.scarperi@gmail.com>
2019-11-02 17:09:42 +0100

    merge works with commented out line 23

--- spr_test.py
+++ spr_test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv("data_gen_v1.csv", sep="|")
-print(df.head())
+df1 = pd.read_csv("data_gen_v1.csv", sep="|", index_col=0)
+#print(df1.head())
 
-df_tomerge = pd.read_csv("lookup.csv", sep=";")
+df2 = pd.read_csv("lookup.csv", sep=";", index_col=0)
 
 
 #Ideal DF
@@ -12,17 +12,22 @@ master = pd.DataFrame(columns = ["gtin", "product_name", "product_desc", "availa
 
 
 # moving data to the master DF
-master["current_price"] = df["Price"]
-master["availability"] = df["In_Stock"]
-master["ref_link"] = df["ref_link"]
-master["product_img"] = df["Product_image"]
-master["shipping_cost"] = df["Shipping"]
-master["product_desc"] =df["Product_desc"]
-master["vendor_name"] = df["Location"]
+master["current_price"] = df1["Price"]
+master["availability"] = df1["In_Stock"]
+master["ref_link"] = df1["ref_link"]
+master["product_img"] = df1["Product_image"]
+master["shipping_cost"] = df1["Shipping"]
+master["product_desc"] =df1["Product_desc"]
+master["vendor_name"] = df1["Location"]
 master["product_rating"] = np.random.randint(1, 6, master.shape[0])
-master["Product_ID"] = df["Product_ID"]
+#master["Product_ID"] = df1["Product_ID"]
 
-#master2 = master.merge(df_tomerge,on="Product_ID")
-master.info()
-#df_tomerge.head(15)
+#masterMerged = pd.merge(master, df2)
+#print(df1.head())
+#print(df2.head())
+#print(masterMerged.head())
+
+theMerge = pd.merge(left=master, right=df2,how='left',left_on='Product_ID', right_on='Product_ID')
+print(theMerge.head())
+
 
