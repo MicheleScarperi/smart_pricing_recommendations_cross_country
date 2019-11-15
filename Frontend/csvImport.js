ec7cbc942dc99492ac29cb16ca8025abb5fd0715
Michele <michele.scarperi@gmail.com>
2019-11-15 19:40:44 +0100

    jesus christ

--- Frontend/csvImport.js
+++ Frontend/csvImport.js
@@ -4,12 +4,18 @@ var fileInput = document.getElementById("csv"),
     readFile = function () {
         var reader = new FileReader();
         reader.onload = function () {
-            document.getElementById('out').innerHTML = reader.result;
+            //document.getElementById('out').innerHTML = reader.result;
+            table = reader.result 
+            csvAsArray = reader.result.csvToArray(",","\n");
+            console.log(csvAsArray)
         };
         // start reading the file. When it is done, calls the onload event defined above.
         reader.readAsBinaryString(fileInput.files[0]);
+        
     };
 
 fileInput.addEventListener('change', readFile);
-console.log("fileInput" + fileInput)
-console.log("readFile" + readFile)
+// Empty array called table that will be populated with the imported csv data.
+var table = [];  
+
+var csvAsArray 
