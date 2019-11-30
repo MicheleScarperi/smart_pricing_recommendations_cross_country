2c1733f33d278befaf5bcf20855bc7b3d17f888b
Michele <michele.scarperi@gmail.com>
2019-11-30 10:37:48 +0100

    cleanup

--- Backend/scraping.py
+++
@@ -1,8 +0,0 @@
-from bs4 import Beautifulsoup as bfs  
-from urllib.request import urlopen
-
-url = "https://www.idealo.de/preisvergleich/OffersOfProduct/6461748_-galaxy-s10-plus-samsung.html"
-content = urlopen(url).read()
-soup = bfs(content)
-
-print (soup.prettify())
