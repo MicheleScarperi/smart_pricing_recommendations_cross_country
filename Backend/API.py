36e015eb70499ac07cf4220b3c0f70532a2fb172
Michele <michele.scarperi@gmail.com>
2019-11-30 10:47:00 +0100

    added comments to api file on usage

--- Backend/API.py
+++ Backend/API.py
@@ -1,10 +1,16 @@
 import flask
 
+# Create 	POST 	Create a new, unique thing
+# Read 	GET 	Read the information about a thing or collection of things
+# Update 	PUT 	Update the information about an existing thing
+# Delete 	DELETE 	Delete a thing
+
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 
 app.run()
