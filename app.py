0bc976402e876acf52eab22a26f272adbb899d22
Michele <michele.scarperi@gmail.com>
2020-01-11 12:07:09 +0100

    new results display functionality

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import render_template
-import connexion
-from flask import request, url_for, jsonify
-from flask_api import FlaskAPI, status, exceptions
+from flask import Flask, request, url_for, jsonify, render_template
 
-app = FlaskAPI(__name__)
+app = Flask(__name__)
 
 products = {
     0: 'iPhone X',
@@ -20,36 +17,30 @@ def product_repr(key):
 
 
 # Create a URL route in our application for "/"
-@app.route('/', methods=['GET', 'POST'])
-def products_list():
-    """
-    List products.
-    :return:        Output from the products dictionary.
-    """
-    if request.method == 'POST':
-        product = str(request.data.get('text', ''))
-        idx = max(products.keys()) + 1
-        products[idx] = product
-        return product_repr(idx), status.HTTP_201_CREATED
-
-    # request.method == 'GET'
-    return [product_repr(idx) for idx in sorted(products.keys())]
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 
-#@app.route('/search', methods=['GET'])
-#def api_all():
- #   search = param.get("key")
-  #  result = products[search]
-   # return jsonify(result)
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.form
+    whatwassearched = [request.form.get('searchTerm'), 'Test1', 'Test2']
 
-@app.route('/search', methods=['GET'])
+    # results = findProduct(whatwassearched)
+
+    return render_template('results.html', result=whatwassearched)
+
+
+@app.route('/search1', methods=['GET'])
 def api_id():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
     # If ID is not provided, display an error in the browser.
     if 'idd' in request.args:
-        idd = int(request.args['idd'])
-        return products[idd]
+        # idd = int(request.args['idd'])
+        # use idd to look up produts that should be returned
+        return request.args['idd']
     else:
         return "Error: No id field provided. Please specify an id."
 
@@ -63,6 +54,7 @@ def api_id():
             searchResults.append(searchResult)
 
     return jsonify(searchResults)
+
 
 # http://0.0.0.0:5000/search?idd=1 this in the url will return Samsung Galaxy
 
